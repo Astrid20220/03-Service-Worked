d57d4fdac20e2e9f104fbe696fd417858dd44756
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-04 14:54:54 -0500

    Service Worked:Fetch

--- js/app.js
+++ js/app.js
@@ -2,5 +2,17 @@
 
 // Detectar si podemos usar Service Workers
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    .then( reg => {
+        setTimeout(() =>{
+
+            reg.sync.register('Posteo-perros');
+            console.log('Se enviaron fotos de perros al server');
+        }, 3000);
+    })
 }
+
+
+fetch('https://reqres.in/api/users')
+    .then(resp => resp.text())
+    .then(console.log)
